5a75bb3d7e0753d1f4295f176038a52389e79664
Ayal Ganem <ayal.ganem@similarweb.com>
2024-06-23 20:23:03 +0300

    add health route

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,12 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,10 +26,8 @@ SECRET_KEY = 'django-insecure-^hy*+1w9=ib&29k1o3yx_eol@4ub*hxe=lffdoxhs@g+5)r6jm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'my-site-lkf2.onrender.com'
-]
-
+# ALLOWED_HOSTS = ['my-site-lkf2.onrender.com']
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME'), 'localhost', '127.0.0.1', '[::1]']
 
 # Application definition
 
@@ -72,21 +71,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_stire',
-        'USER': 'my_stire_user',
-        'PASSWORD': 'vKI692qjfdsa8WEUIDnydeVvTdzYYxAh',
-        'HOST': 'dpg-cps3oqij1k6c738h14p0-a.oregon-postgres.render.com',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -106,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -117,7 +106,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
